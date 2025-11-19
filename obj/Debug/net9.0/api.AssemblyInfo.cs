796ca14992d6a6e67ad4fd7e70de182c7ecc3b80
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-11-19 17:00:48 -0300

    testando classe phone

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024b174cf8c93ba2fcc89611e6e970be69c9f9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3aec51d9aad78662bc52be3c965a4e104fde9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
